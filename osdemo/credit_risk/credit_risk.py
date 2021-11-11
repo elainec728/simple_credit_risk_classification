8a62673f7e6acf0bc39c458e5e588bc361b338cf
wenrui cao <wc1172@nyu.edu>
2021-11-11 22:13:09 +0800

    response data type

--- osdemo/credit_risk/credit_risk.py
+++ osdemo/credit_risk/credit_risk.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import pandas as pd
 import json
-
-from sklearn.linear_model import LogisticRegression
-from sklearn import metrics
-from sklearn.preprocessing import LabelEncoder, KBinsDiscretizer, MinMaxScaler
-import pickle
-import numpy as np
 import logging
+import os
+import pickle
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder, KBinsDiscretizer, MinMaxScaler
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 file_path = os.path.join(BASE_DIR, 'data', "german_credit_data_biased_training.csv")
@@ -19,26 +18,30 @@ MODEL_PATH = os.path.join(BASE_DIR, 'credit_risk', 'model.pkl')
 
 def scoring(payload_scoring, model, X_prs, y_prs):
     logging.info('starting scoring')
-    js=json.loads(json.dumps(payload_scoring.get('input_data','')))
-    if isinstance(js,str):
-        js=js.replace('\'','\",')
-        js=json.loads(js)
+    js = json.loads(json.dumps(payload_scoring.get('input_data', '')))
+    if isinstance(js, str):
+        js = js.replace('\'', '\",')
+        js = json.loads(js)
     fields = js.get('fields', []) + ["PREDICTION", "PROBABILITY"]
     X_test = np.array(js.get('values', []))
     # X_test_processed = np.empty((X_test.shape[0], X_test.shape[1] - 1))
     X_test_processed = []
+
+    X_test_right_format=[[] for i in range(X_test.shape[0])]
     for i in range(X_test.shape[1] - 1):
         if isinstance(X_prs[i], LabelEncoder):
             x = X_prs[i].transform(X_test[:, i].astype(str).reshape((-1, 1)))
             X_test_processed.append(list(x.reshape((-1,))))
+            [X_test_right_format[j].append(X_test[j,i]) for j in range(len(x))]
         else:
             x = X_prs[i].transform(X_test[:, i].reshape((-1, 1)))
             X_test_processed.append(list(x.reshape((-1,))))
+            [X_test_right_format[j].append(int(X_test[j,i])) for j in range(len(x))]
     X_test_processed = np.array(X_test_processed).T
     y_prob = model.predict_proba(X_test_processed)
     y_pred = model.predict(X_test_processed)
 
-    values = [list(X_test[i, :].reshape((-1))) +
+    values = [X_test_right_format[i] +
               list(y_prs.inverse_transform(y_pred[i].reshape((-1)))) +
               [list(y_prob[i, :])] for i in range(len(X_test))]
     return {"fields": fields,
@@ -55,7 +58,9 @@ def training(X_train, y_train):
 
 def preprocess(df):
     preprocess_config = {'LOANDURATION': KBinsDiscretizer(n_bins=5, encode='ordinal'), 'LOANAMOUNT': MinMaxScaler(),
-                         'AGE': KBinsDiscretizer(n_bins=5, encode='ordinal')}
+                         'AGE': KBinsDiscretizer(n_bins=5, encode='ordinal'), 'INSTALLMENTPERCENT': MinMaxScaler(),
+                         'CURRENTRESIDENCEDURATION': MinMaxScaler(), 'EXISTINGCREDITSCOUNT': MinMaxScaler(),
+                         'DEPENDENTS': MinMaxScaler()}
     preprocessors = []
     columns = df.columns.tolist()[:-1]
     X_train = []
@@ -105,9 +110,9 @@ if __name__ == '__main__':
 
     sample_test_payload = {
         'fields': ['CHECKINGSTATUS', 'LOANDURATION', 'CREDITHISTORY', 'LOANPURPOSE', 'LOANAMOUNT',
-                       'EXISTINGSAVINGS', 'EMPLOYMENTDURATION', 'INSTALLMENTPERCENT', 'SEX', 'OTHERSONLOAN',
-                       'CURRENTRESIDENCEDURATION', 'OWNSPROPERTY', 'AGE', 'INSTALLMENTPLANS', 'HOUSING',
-                       'EXISTINGCREDITSCOUNT', 'JOB', 'DEPENDENTS', 'TELEPHONE', 'FOREIGNWORKER'], 'values': [
+                   'EXISTINGSAVINGS', 'EMPLOYMENTDURATION', 'INSTALLMENTPERCENT', 'SEX', 'OTHERSONLOAN',
+                   'CURRENTRESIDENCEDURATION', 'OWNSPROPERTY', 'AGE', 'INSTALLMENTPLANS', 'HOUSING',
+                   'EXISTINGCREDITSCOUNT', 'JOB', 'DEPENDENTS', 'TELEPHONE', 'FOREIGNWORKER'], 'values': [
             ['0_to_200', 31, 'credits_paid_to_date', 'other', 1889, '100_to_500', 'less_1', 3, 'female', 'none', 3,
              'savings_insurance', 32, 'none', 'own', 1, 'skilled', 1, 'none', 'yes'],
             ['less_0', 18, 'credits_paid_to_date', 'car_new', 462, 'less_100', '1_to_4', 2, 'female', 'none', 2,
@@ -116,4 +121,4 @@ if __name__ == '__main__':
     with open(MODEL_PATH, 'rb') as f:
         res = pickle.load(f)
 
-    print(scoring({'input_data':sample_test_payload}, *res))
+    print(scoring({'input_data': sample_test_payload}, *res))
